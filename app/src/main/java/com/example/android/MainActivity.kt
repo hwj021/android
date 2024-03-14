81c944d35b1661eba7295bc17f3929fccd15df40
hwj021 <patyyxyz@gmail.com>
2024-03-14 20:55:48 +0100

    Wypisanie Naciśniętych Wartosci - Początek Kalkulatora

--- app/src/main/java/com/example/android/MainActivity.kt
+++ app/src/main/java/com/example/android/MainActivity.kt
@@ -1,46 +1,93 @@
 package com.example.android
 
 import android.os.Bundle
-import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var textView0: TextView
-    private lateinit var textView1: TextView
-    private lateinit var textView2: TextView
-    private lateinit var textView3: TextView
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        textView0 = findViewById(R.id.textView0)
-        textView0.text = "Witam"
-
-
-        textView1 = findViewById(R.id.textView1)
-        textView2 = findViewById(R.id.textView2)
-        textView3 = findViewById(R.id.textView3)
-
-        val myButton: Button = findViewById(R.id.myButton)
         val button1: Button = findViewById(R.id.button1)
         val button2: Button = findViewById(R.id.button2)
+        val button3: Button = findViewById(R.id.button3)
+        val button4: Button = findViewById(R.id.button4)
+        val button5: Button = findViewById(R.id.button5)
+        val button6: Button = findViewById(R.id.button6)
+        val button7: Button = findViewById(R.id.button7)
+        val button8: Button = findViewById(R.id.button8)
+        val button9: Button = findViewById(R.id.button9)
+        val button10: Button = findViewById(R.id.button10)
+        val button11: Button = findViewById(R.id.button11)
+        val button12: Button = findViewById(R.id.button12)
+        val button13: Button = findViewById(R.id.button13)
+        val button14: Button = findViewById(R.id.button14)
+        val button15: Button = findViewById(R.id.button15)
 
-        myButton.setOnClickListener {
-            textView1.text = "Hello World"
-        }
+        val textView: TextView = findViewById(R.id.textView)
 
         button1.setOnClickListener {
-            textView2.text = "Michal Pacholski"
+            textView.append("1")
         }
 
         button2.setOnClickListener {
-            textView3.text = "Goodbye"
+            textView.append("2")
+        }
+
+        button3.setOnClickListener {
+            textView.append("3")
+        }
+
+        button4.setOnClickListener {
+            textView.append("4")
+        }
+
+        button5.setOnClickListener {
+            textView.append("5")
+        }
+
+        button6.setOnClickListener {
+            textView.append("6")
+        }
+
+        button7.setOnClickListener {
+            textView.append("7")
+        }
+
+        button8.setOnClickListener {
+            textView.append("8")
+        }
+
+        button9.setOnClickListener {
+            textView.append("9")
+        }
+
+        button10.setOnClickListener {
+            textView.append("0")
+        }
+
+        button11.setOnClickListener {
+            textView.append("+")
+        }
+
+        button12.setOnClickListener {
+            textView.append("-")
+        }
+
+        button13.setOnClickListener {
+            textView.append("*")
+        }
+
+        button14.setOnClickListener {
+            textView.append("/")
+        }
+
+        button15.setOnClickListener {
+            textView.text = ""
         }
     }
 }
-
 
